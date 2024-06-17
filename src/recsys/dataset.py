b080d3d0248182d373cd9e0f78b3a069b1a01b56
mkrastev <matey_krastev2@hotmail.com>
2024-06-17 11:31:56 +0200

    Implement basic category classifier.

--- src/recsys/dataset.py
+++ src/recsys/dataset.py
@@ -12,6 +12,7 @@ from ebrec.utils._constants import (
     DEFAULT_ARTICLE_ID_COL,
     DEFAULT_BODY_COL,
     DEFAULT_CATEGORY_STR_COL,
+    DEFAULT_CATEGORY_COL,
     DEFAULT_CLICKED_ARTICLES_COL,
     DEFAULT_HISTORY_ARTICLE_ID_COL,
     DEFAULT_IMPRESSION_ID_COL,
@@ -100,9 +101,11 @@ class NewsDataset(Dataset):
             "history_size": self.history_size,
             "padding_value": self.padding_value,
             "max_labels": self.max_labels,
+            "max_categories": self.max_categories
         }
 
         np.save(path + "/lookup_matrix.npy", self.lookup_matrix)
+        np.save(path + "/aux_cat_lookup_matrix.npy", self.aux_cat_lookup_matrix)
         with open(path + "/parameters.json", "w") as f:
             json.dump(data, f)
         self.behaviors.write_parquet(path + "/behaviors.parquet")
@@ -119,9 +122,10 @@ class NewsDataset(Dataset):
             dataset.history_size = data["history_size"]
             dataset.padding_value = data["padding_value"]
             dataset.max_labels = data["max_labels"]
+            dataset.max_categories = data["max_categories"]
 
         dataset.lookup_matrix = np.load(path + "/lookup_matrix.npy")
-
+        dataset.aux_cat_lookup_matrix = np.load(path + "/aux_cat_lookup_matrix.npy")
         dataset.behaviors = pl.read_parquet(path + "/behaviors.parquet")
         dataset.history = pl.read_parquet(path + "/history.parquet")
         dataset.articles = pl.read_parquet(path + "/articles.parquet")
@@ -146,10 +150,47 @@ class NewsDataset(Dataset):
                 history_size=history_size,
                 padding_value=padding_value,
                 enable_warning=False,
-            ).collect()
+            )
+            .collect()
         )
 
     def _prepare_training_data(self, embeddings_path=None):
+        assert (
+            embeddings_path is not None
+        ), "You need to provide a path to the embeddings file."
+        embeddings = pl.read_parquet(embeddings_path)
+
+        self.articles = (
+            self.articles.lazy()
+            .join(embeddings.lazy(), on=DEFAULT_ARTICLE_ID_COL, how="inner")
+            .rename({embeddings.columns[-1]: DEFAULT_TOKENS_COL})
+            .cast({DEFAULT_CATEGORY_STR_COL: pl.Categorical})
+            .with_columns(
+                pl.col(DEFAULT_CATEGORY_STR_COL)
+                .to_physical()
+                .alias(DEFAULT_CATEGORY_COL)
+            )
+            .collect()
+        )
+
+        # ArticleID2Vec lookup
+        article_dict = create_lookup_dict(
+            self.articles.select(DEFAULT_ARTICLE_ID_COL, DEFAULT_TOKENS_COL),
+            key=DEFAULT_ARTICLE_ID_COL,
+            value=DEFAULT_TOKENS_COL,
+        )
+
+        # Create the lookup indexes and matrix
+        self.lookup_indexes, self.lookup_matrix = create_lookup_objects(
+            article_dict, unknown_representation="zeros"
+        )
+
+        self.aux_cat_lookup_matrix = (
+            self.articles[DEFAULT_CATEGORY_COL].to_numpy().astype(np.uint8)
+        )
+        self.max_categories = self.aux_cat_lookup_matrix.max().item() + 1
+
+        # Prepare the actual training data
         self.behaviors = self.behaviors.collect()
 
         self.data = (
@@ -163,42 +204,25 @@ class NewsDataset(Dataset):
             .pipe(create_binary_labels_column, seed=42, label_col=DEFAULT_LABELS_COL)
             .pipe(sort_and_select, n=self.max_labels)
             .with_columns(pl.col(DEFAULT_LABELS_COL).list.len().alias(N_SAMPLES_COL))
+            .with_columns()
         )
 
-        assert (
-            embeddings_path is not None
-        ), "You need to provide a path to the embeddings file."
-        embeddings = pl.read_parquet(embeddings_path)
-
-        self.articles = (
-            self.articles.lazy()
-            .join(embeddings.lazy(), on=DEFAULT_ARTICLE_ID_COL, how="inner")
-            .rename({embeddings.columns[-1]: DEFAULT_TOKENS_COL})
-            .collect()
+        self.data = (
+            self.data.lazy()
+            .pipe(
+                map_list_article_id_to_value,
+                behaviors_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
+                mapping=self.lookup_indexes,
+                fill_nulls=[0],
+            )
+            .pipe(
+                map_list_article_id_to_value,
+                behaviors_column=DEFAULT_INVIEW_ARTICLES_COL,
+                mapping=self.lookup_indexes,
+                fill_nulls=[0],
+            )
+            .collect(streaming=True)
         )
-
-        article_dict = create_lookup_dict(
-            self.articles.select(DEFAULT_ARTICLE_ID_COL, DEFAULT_TOKENS_COL),
-            key=DEFAULT_ARTICLE_ID_COL,
-            value=DEFAULT_TOKENS_COL,
-        )
-
-        self.lookup_indexes, self.lookup_matrix = create_lookup_objects(
-            article_dict, unknown_representation="zeros"
-        )
-
-        # self.lookup_indexes = {i: val.item() for i, val in self.lookup_indexes.items()}
-        self.data = self.data.lazy().pipe(
-            map_list_article_id_to_value,
-            behaviors_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
-            mapping=self.lookup_indexes,
-            fill_nulls=[0],
-        ).pipe(
-            map_list_article_id_to_value,
-            behaviors_column=DEFAULT_INVIEW_ARTICLES_COL,
-            mapping=self.lookup_indexes,
-            fill_nulls=[0],
-        ).collect(streaming=True)
 
         self.data = PolarsDataFrameWrapper(self.data)
 
@@ -219,18 +243,23 @@ class NewsDataset(Dataset):
         """
 
         batch = self.data[index]
-        history_input = self.lookup_matrix[
-            batch[DEFAULT_HISTORY_ARTICLE_ID_COL].to_list()
-        ]
+        _hist = batch[DEFAULT_HISTORY_ARTICLE_ID_COL].to_list()
+
+        # Grab history and candidate vectors
+        history_input = self.lookup_matrix[_hist]
         candidate_input = self.lookup_matrix[
             batch[DEFAULT_INVIEW_ARTICLES_COL].to_list()
         ]
         # =>
-        history_input = torch.tensor(history_input).squeeze()
-        candidate_input = torch.tensor(candidate_input).squeeze()
+        history_input = torch.from_numpy(history_input).squeeze()
+        candidate_input = torch.from_numpy(candidate_input).squeeze()
         y = torch.tensor(batch[DEFAULT_LABELS_COL], dtype=torch.float32).squeeze()
+
+        category = self.aux_cat_lookup_matrix[_hist]
+
+        category = torch.from_numpy(category).long().squeeze()
         # ========================
-        return history_input, candidate_input, y
+        return history_input, candidate_input, category, y
 
 
 class NewsDataModule(LightningDataModule):
@@ -278,9 +307,7 @@ class NewsDataModule(LightningDataModule):
 
         # Download the embeddings
         embeddings_url = CHALLENGE_DATASET[self.embeddings]
-        embeddings_folder = os.path.join(
-            self.root_path, self.embeddings
-        )
+        embeddings_folder = os.path.join(self.root_path, self.embeddings)
         if not os.path.exists(embeddings_folder):
             os.makedirs(embeddings_folder, exist_ok=True)
             filename = download_file(
@@ -314,73 +341,76 @@ class NewsDataModule(LightningDataModule):
         else:
             self.data_path = savefolder
 
-    def setup(self, stage: str):
+    def setup(self, stage: str = None):
         match stage:
             case "fit" | "validation" | None:
                 # Load the training data
-                save_dir = os.path.join(self.data_path, "train", "preprocessed")
-                if os.path.exists(save_dir):
-                    self.train_dataset = NewsDataset.from_preprocessed(save_dir)
+                if not hasattr(self, "train_dataset"):
+                    save_dir = os.path.join(self.data_path, "train", "preprocessed")
+                    if os.path.exists(save_dir):
+                        self.train_dataset = NewsDataset.from_preprocessed(save_dir)
 
-                else:
-                    df_behaviors, df_history, df_articles = load_data(
-                        self.data_path, split="train"
-                    )
-                    self.train_dataset = NewsDataset(
-                        tokenizer=self.tokenizer,
-                        behaviors=df_behaviors,
-                        history=df_history,
-                        articles=df_articles,
-                        history_size=self.history_size,
-                        max_labels=self.max_labels,
-                        padding_value=self.padding_value,
-                        max_length=self.max_length,
-                        embeddings_path=self.embeddings_path,
-                    )
-                    os.makedirs(save_dir, exist_ok=True)
-                    self.train_dataset.save_preprocessed(save_dir)
+                    else:
+                        df_behaviors, df_history, df_articles = load_data(
+                            self.data_path, split="train"
+                        )
+                        self.train_dataset = NewsDataset(
+                            tokenizer=self.tokenizer,
+                            behaviors=df_behaviors,
+                            history=df_history,
+                            articles=df_articles,
+                            history_size=self.history_size,
+                            max_labels=self.max_labels,
+                            padding_value=self.padding_value,
+                            max_length=self.max_length,
+                            embeddings_path=self.embeddings_path,
+                        )
+                        os.makedirs(save_dir, exist_ok=True)
+                        self.train_dataset.save_preprocessed(save_dir)
 
-                # Load the validation data
-                save_dir = os.path.join(self.data_path, "validation", "preprocessed")
-                if os.path.exists(save_dir):
-                    self.val_dataset = NewsDataset.from_preprocessed(save_dir)
-                else:
-                    df_behaviors, df_history, df_articles = load_data(
-                        self.data_path, split="validation"
-                    )
-                    self.val_dataset = NewsDataset(
-                        tokenizer=self.tokenizer,
-                        behaviors=df_behaviors,
-                        history=df_history,
-                        articles=df_articles,
-                        history_size=self.history_size,
-                        max_labels=self.max_labels,
-                        padding_value=self.padding_value,
-                        max_length=self.max_length,
-                        embeddings_path=self.embeddings_path,
-                    )
-                    os.makedirs(save_dir, exist_ok=True)
-                    self.val_dataset.save_preprocessed(save_dir)
+                if not hasattr(self, "val_dataset"):
+                    # Load the validation data
+                    save_dir = os.path.join(self.data_path, "validation", "preprocessed")
+                    if os.path.exists(save_dir):
+                        self.val_dataset = NewsDataset.from_preprocessed(save_dir)
+                    else:
+                        df_behaviors, df_history, df_articles = load_data(
+                            self.data_path, split="validation"
+                        )
+                        self.val_dataset = NewsDataset(
+                            tokenizer=self.tokenizer,
+                            behaviors=df_behaviors,
+                            history=df_history,
+                            articles=df_articles,
+                            history_size=self.history_size,
+                            max_labels=self.max_labels,
+                            padding_value=self.padding_value,
+                            max_length=self.max_length,
+                            embeddings_path=self.embeddings_path,
+                        )
+                        os.makedirs(save_dir, exist_ok=True)
+                        self.val_dataset.save_preprocessed(save_dir)
 
             case _:
                 # Otherwise, test.
-                self._download_test()
-                df_behaviors, df_history, df_articles = load_data(
-                    self.data_path, split="test"
-                )
-                self.test_dataset = NewsDataset(
-                    tokenizer=self.tokenizer,
-                    behaviors=df_behaviors,
-                    history=df_history,
-                    articles=df_articles,
-                    history_size=self.history_size,
-                    max_labels=self.max_labels,
-                    padding_value=self.padding_value,
-                    max_length=self.max_length,
-                    embeddings_path=self.embeddings_path,
-                )
-                os.makedirs(save_dir, exist_ok=True)
-                self.test_dataset.save_preprocessed(save_dir)
+                if not hasattr(self, "test_dataset"):
+                    self._download_test()
+                    df_behaviors, df_history, df_articles = load_data(
+                        self.data_path, split="test"
+                    )
+                    self.test_dataset = NewsDataset(
+                        tokenizer=self.tokenizer,
+                        behaviors=df_behaviors,
+                        history=df_history,
+                        articles=df_articles,
+                        history_size=self.history_size,
+                        max_labels=self.max_labels,
+                        padding_value=self.padding_value,
+                        max_length=self.max_length,
+                        embeddings_path=self.embeddings_path,
+                    )
+                    os.makedirs(save_dir, exist_ok=True)
+                    self.test_dataset.save_preprocessed(save_dir)
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(
@@ -436,6 +466,7 @@ def load_data(
     df_articles = pl.scan_parquet(data_path + "/articles.parquet")
 
     return df_behaviors, df_history, df_articles
+
 
 def map_list_article_id_to_value(
     behaviors: pl.LazyFrame,
@@ -546,9 +577,7 @@ def map_list_article_id_to_value(
             pl.col(behaviors_column).fill_null(fill_nulls)
         )
     # =>
-    select_column = (
-        select_column.group_by(GROUPBY_ID).agg(behaviors_column)
-    )
+    select_column = select_column.group_by(GROUPBY_ID).agg(behaviors_column)
     return (
         behaviors.drop(behaviors_column)
         .join(select_column, on=GROUPBY_ID, how="left")
@@ -571,7 +600,9 @@ def sort_and_select(
     #     a.append(x[idx])
     #     b.append(df[inview_col][i][idx])
 
-    idx = df[labels_col].list.eval(pl.element().arg_sort().take([-1]+list(range(n-1))).shuffle(),parallel=True)
+    idx = df[labels_col].list.eval(
+        pl.element().arg_sort().take([-1] + list(range(n - 1))).shuffle(), parallel=True
+    )
     a = df[labels_col].list.gather(idx)
     b = df[inview_col].list.gather(idx)
 
--- src/recsys/model.py
+++ src/recsys/model.py
@@ -14,7 +14,7 @@ from ebrec.evaluation.metrics_protocols import (
 )
 
 # Setting to get more matmul performance on Tensor Core capable machines.
-torch.set_float32_matmul_precision('medium')
+torch.set_float32_matmul_precision("medium")
 
 
 def apply_softmax_crossentropy(logits, one_hot_targets, epsilon=1e-10):
@@ -32,23 +32,23 @@ def apply_softmax_crossentropy(logits, one_hot_targets, epsilon=1e-10):
     """
 
     # Split logits and one-hot targets according to repeats
-    #split_logits = torch.split(logits, repeats.tolist())
-    #split_targets = torch.split(one_hot_targets, repeats.tolist())
+    # split_logits = torch.split(logits, repeats.tolist())
+    # split_targets = torch.split(one_hot_targets, repeats.tolist())
 
     # Determine the maximum length for padding
-    #max_len = max(repeats)
-    
+    # max_len = max(repeats)
+
     # Pad logits and one-hot targets, then stack them
-    #padded_logits = torch.stack([F.pad(segment, (0, max_len - segment.size(0)), 'constant', float('-inf')) for segment in split_logits])
-    #padded_targets = torch.stack([F.pad(segment, (0, max_len - segment.size(0)), 'constant', 0) for segment in split_targets])
-    
+    # padded_logits = torch.stack([F.pad(segment, (0, max_len - segment.size(0)), 'constant', float('-inf')) for segment in split_logits])
+    # padded_targets = torch.stack([F.pad(segment, (0, max_len - segment.size(0)), 'constant', 0) for segment in split_targets])
+
     # Apply softmax to logits and add epsilon to avoid NaNs
     softmaxed_logits = F.softmax(logits, dim=-1)
     log_softmaxed_logits = torch.log(softmaxed_logits + epsilon)
 
     # Calculate cross-entropy loss
     losses = -torch.sum(one_hot_targets * log_softmaxed_logits, dim=-1)
-    
+
     # Mask out the padded positions
     mask = (one_hot_targets.sum(dim=-1) > 0).float()
     masked_losses = losses * mask
@@ -81,6 +81,7 @@ class UserEncoder(nn.Module):
         user_embedding = torch.sum(history * att_weight, dim=1)
         return user_embedding
 
+
 class MTRec(nn.Module):
     """The main prediction model for the multi-task recommendation system, as described in the paper by ..."""
 
@@ -89,13 +90,13 @@ class MTRec(nn.Module):
 
         self.W = nn.Linear(hidden_dim, hidden_dim)
         self.q = nn.Parameter(torch.randn(hidden_dim))
-        #self.transformer_hist_layer = nn.TransformerEncoderLayer(d_model=hidden_dim, nhead=8)
-        #self.transformer_hist = nn.TransformerEncoder(self.transformer_hist_layer, num_layers=2)
-        #self.W_cand = nn.Linear(hidden_dim, hidden_dim)
-        #self.W_cand2 = nn.Linear(hidden_dim, hidden_dim)
-        #self.dropout = nn.Dropout(0.1)
-        #self.layer_norm = nn.LayerNorm(hidden_dim)
-        #self.norm2 = nn.LayerNorm(hidden_dim)
+        # self.transformer_hist_layer = nn.TransformerEncoderLayer(d_model=hidden_dim, nhead=8)
+        # self.transformer_hist = nn.TransformerEncoder(self.transformer_hist_layer, num_layers=2)
+        # self.W_cand = nn.Linear(hidden_dim, hidden_dim)
+        # self.W_cand2 = nn.Linear(hidden_dim, hidden_dim)
+        # self.dropout = nn.Dropout(0.1)
+        # self.layer_norm = nn.LayerNorm(hidden_dim)
+        # self.norm2 = nn.LayerNorm(hidden_dim)
 
     def forward(self, history, candidates):
         """
@@ -107,17 +108,19 @@ class MTRec(nn.Module):
         """
 
         # print(f"{candidates.shape=}")
-        #history = self.transformer_hist(history)
+        # history = self.transformer_hist(history)
         att = self.q * F.tanh(self.W(history))
         att_weight = F.softmax(att, dim=1)
         # print(f"{att_weight.shape=}")
 
-        user_embedding = torch.sum(history * att_weight, dim = 1)
+        user_embedding = torch.sum(history * att_weight, dim=1)
         # print(f"{user_embedding.shape=}")
         # print(f"{user_embedding.unsqueeze(-1).shape=}")
-        #candidates = self.norm2(candidates + self.W_cand2(self.layer_norm(self.dropout(F.relu(self.W_cand(candidates))))))
+        # candidates = self.norm2(candidates + self.W_cand2(self.layer_norm(self.dropout(F.relu(self.W_cand(candidates))))))
 
-        score = torch.bmm(candidates, user_embedding.unsqueeze(-1))/torch.sqrt(candidates.size(-1)) # B x M x 1
+        score = torch.bmm(candidates, user_embedding.unsqueeze(-1)) / torch.sqrt(
+            candidates.size(-1)
+        )  # B x M x 1
         # print(score.shape)
         return score.squeeze(-1)
 
@@ -125,11 +128,30 @@ class MTRec(nn.Module):
         n_news = len(batch_news) // bz
         reshaped_batch = batch_news.reshape(bz, n_news, -1)
         return reshaped_batch
-    
+
+
 class NLLLoss(nn.Module):
     def forward(self, preds, target):
         preds = preds.sigmoid()
-        return - torch.log(preds.where(target==1, -torch.inf).exp().sum(dim=-1, keepdims=True) / (preds.exp().sum(dim=-1, keepdims=True))).mean()
+        return -torch.log(
+            preds.where(target == 1, -torch.inf).exp().sum(dim=-1, keepdims=True)
+            / (preds.exp().sum(dim=-1, keepdims=True))
+        ).mean()
+
+
+class CategoryEncoder(nn.Module):
+    def __init__(self, hidden_dim, n_categories=5):
+        super(CategoryEncoder, self).__init__()
+        self.linear = nn.Linear(hidden_dim, n_categories)
+
+    def forward(self, history):
+        """
+        B - batch size (keep in mind we use an unusual mini-batch approach)
+        H - history size (number of articles in the history, usually 30)
+        D - hidden size (768)
+        history: B x H x D
+        """
+        return self.linear(history)
 
 
 class MultitaskRecommender(LightningModule):
@@ -137,7 +159,16 @@ class MultitaskRecommender(LightningModule):
     The main prediction model for the multi-task recommendation system we implement.
     """
 
-    def __init__(self, hidden_dim, nhead=8, num_layers=4, lr=1e-2, wd=0.0, **kwargs):
+    def __init__(
+        self,
+        hidden_dim,
+        nhead=8,
+        num_layers=4,
+        n_categories=5,
+        lr=1e-2,
+        wd=0.0,
+        **kwargs,
+    ):
         super().__init__()
 
         self.save_hyperparameters()
@@ -148,6 +179,8 @@ class MultitaskRecommender(LightningModule):
 
         self.user_encoder = UserEncoder(hidden_dim)
         self.transformer = nn.TransformerEncoder(transformer, num_layers=num_layers)
+        self.category_encoder = CategoryEncoder(hidden_dim, n_categories=n_categories)
+
         self.predictions = []
         self.labels = []
         self.metric_evaluator = MetricEvaluator(
@@ -163,11 +196,11 @@ class MultitaskRecommender(LightningModule):
                 F1Score(),
             ],
         )
-        
+
         # from torchmetrics.retrieval import RetrievalAUROC
-        from torchmetrics.classification import MulticlassAccuracy
         from torchmetrics.classification import MultilabelAccuracy
         from torchmetrics.classification import MultilabelAUROC
+
         self.accuracy = MultilabelAccuracy(num_labels=5)
         self.auroc = MultilabelAUROC(num_labels=5)
 
@@ -183,7 +216,7 @@ class MultitaskRecommender(LightningModule):
         # [0, 0, 0, 0, 1]
         # [0, 0, 0, 0, 1]
         # [0, 0, 0, 0, 1]
-        
+
         # [0.1, 0.2, 0.3, 0.4, 0.5]
         # [0.1, 0.2, 0.3, 0.4, 0.5]
         # [0.1, 0.2, 0.3, 0.4, 0.5]
@@ -192,31 +225,31 @@ class MultitaskRecommender(LightningModule):
 
         # self.criterion = NLLLoss()
         self.criterion = nn.CrossEntropyLoss()
+        self.category_loss = nn.CrossEntropyLoss()
 
     def configure_optimizers(self):
-        print(f'Learning rate: {self.hparams.lr}')
-        return torch.optim.AdamW(
-            self.parameters(), lr=self.hparams.lr
-        )
+        print(f"Learning rate: {self.hparams.lr}")
+        return torch.optim.AdamW(self.parameters(), lr=self.hparams.lr)
 
     def forward(self, history, candidates):
         """
         LEGEND:
         B - batch size (keep in mind we use an unusual mini-batch approach)
         H - history size (number of articles in the history, usually 30)
-        C - candidate length 
+        C - candidate length
         D - hidden size (768)
         history:    B x H x D
         candidates: B x C x D
-        
+
         Returns:
         B x C scores
         """
         # Implement a baseline: LinearRegression, SVM?
         # Suggestion: Concatenate both vectors and pass them through a linear layer? (Only if we have time)
-        # Maybe integrate our own BERT and finetune it? 
+        # Maybe integrate our own BERT and finetune it?
         # history = self.transformer(history)
         # user_embedding = self.transformer(history)
+        # user_embedding = torch.sum(history * user_embedding.softmax(dim=1), dim=1)
         # user_embedding = (user_embedding.softmax(dim=1) * user_embedding).sum(dim=1)
         user_embedding = self.user_encoder(history)
         # Normalization in order to reduce the variance of the dot product
@@ -228,17 +261,26 @@ class MultitaskRecommender(LightningModule):
     def on_train_epoch_start(self) -> None:
         super().on_train_epoch_start()
         self.param = next(iter(self.parameters())).detach().clone()
+
     def on_train_epoch_end(self) -> None:
         super().on_train_epoch_end()
         self.param = self.param - next(iter(self.parameters())).detach().clone()
         self.log("param_change", self.param.abs().mean(), prog_bar=True)
 
     def training_step(self, batch, batch_idx):
-        history, candidates, labels = batch
+        history, candidates, category, labels = batch
         scores = self(history, candidates)
 
-        loss = self.criterion(scores, labels)
+        # News Ranking Loss
+        news_ranking_loss = self.criterion(scores, labels)
+        category_scores = self.category_encoder(history)
 
+        category = torch.nn.functional.one_hot(
+            category, num_classes=self.hparams.n_categories
+        ).float()
+        category_loss = self.category_loss(category_scores, category)
+
+        loss = news_ranking_loss + category_loss
         self.log("train_loss", loss, prog_bar=True)
         return loss
 
@@ -249,15 +291,15 @@ class MultitaskRecommender(LightningModule):
         self.labels.clear()
 
     def validation_step(self, batch, batch_idx):
-        history, candidates, labels = batch
+        history, candidates, category, labels = batch
         scores = self(history, candidates)
 
         loss = self.criterion(scores, labels)
-        
+
         accuracy = self.accuracy(scores, labels)
         auroc = self.auroc(scores, labels.long())
-        # self.log("val_accuracy", accuracy, prog_bar=True) 
-        self.log("val_auroc", auroc, prog_bar=True) 
+        self.log("val_accuracy", accuracy, prog_bar=True)
+        self.log("val_auroc", auroc, prog_bar=True)
         self.log("val_loss", loss, prog_bar=True)
         self.predictions.append(scores.detach().cpu().flatten().float().numpy())
         self.labels.append(labels.detach().cpu().flatten().float().numpy())
@@ -268,4 +310,11 @@ class MultitaskRecommender(LightningModule):
         self.log_dict(metrics.evaluations)
 
     def test_step(self, batch, batch_idx):
-        return self.validation_step(batch, batch_idx)
+        history, candidates, category, _ = batch
+        res = []
+        for inview in candidates:
+            scores = self(history, inview.unsqueeze(0))
+            indices = torch.argsort(scores, descending=True)
+            res.append(indices.tolist())
+
+        return res
