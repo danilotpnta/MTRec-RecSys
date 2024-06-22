604ccad99e37a8a1bd3c86d52eb575ab44c21c6f
Stefan Vasilev <scur0998@int6.local.snellius.surf.nl>
2024-06-22 23:19:34 +0200

    automatic optimization

--- src/recsys/model.py
+++ src/recsys/model.py
@@ -79,7 +79,7 @@ class BERTMultitaskRecommender(LightningModule):
         self, epochs=10, lr=1e-3, wd=0.0, steps_per_epoch=None, n_categories=5, **kwargs
     ):
         super().__init__()
-        self.automatic_optimization = kwargs.get("use_gradient_surgery", True)
+        self.automatic_optimization = not kwargs.get("use_gradient_surgery", True)
         print("automatic optimization:", self.automatic_optimization)
         self.save_hyperparameters(ignore="embeddings")
         self.predictions = []
